c7e9a8904ce725b27c7569bc76336c230e829d07
Demian <61812941+DemianAdam@users.noreply.github.com>
2026-01-28 17:42:18 -0300

    Added gh-pages and dev routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,25 @@
-  import { StrictMode } from 'react';
-  import { createRoot } from 'react-dom/client';
-  import App from './App.tsx';
-  import './index.css';
-  import { BrowserRouter } from "react-router-dom";
-  import { ConvexReactClient } from "convex/react";
-  import { ConvexAuthProvider } from "@convex-dev/auth/react";
-  import { ConvexQueryCacheProvider } from 'convex-helpers/react/cache';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { BrowserRouter, HashRouter } from "react-router-dom";
+import { ConvexReactClient } from "convex/react";
+import { ConvexAuthProvider } from "@convex-dev/auth/react";
+import { ConvexQueryCacheProvider } from 'convex-helpers/react/cache';
 
-  const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string, { verbose: false, });
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string, { verbose: false, });
 
-  createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-      <ConvexAuthProvider client={convex}>
-        <ConvexQueryCacheProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </ConvexQueryCacheProvider>
-      </ConvexAuthProvider>
-    </StrictMode>
-  );
+const Router = true ? HashRouter : BrowserRouter;
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ConvexAuthProvider client={convex}>
+      <ConvexQueryCacheProvider>
+        <Router>
+          <App />
+        </Router>
+      </ConvexQueryCacheProvider>
+    </ConvexAuthProvider>
+  </StrictMode>
+);
